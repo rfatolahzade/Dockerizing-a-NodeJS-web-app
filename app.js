52908dcbb05ee394f8d6774d13c1cdc95a5765a6
rfinland <r.finland88@gmail.com>
2022-01-11 19:25:32 +0330

    init

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var usersRouter = require("./routes/users");
+var app = express();
+
+
+app.get("/Exception", function mainHandler(req, res) {
+  throw new Error("An exception occured!");
+});
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+
+app.use("/users", usersRouter);
+app.get("/Hello", function rootHandler(req, res) {
+  res.end("Hello world!!");
+});
+
+app.use("/POW", function (req, res, next) {
+  res.send(Math.pow(req.query.a, req.query.b).toString());
+});
+var listener = app.listen(8080, function () {
+  console.log("Listening on port " + listener.address().port);
+});
