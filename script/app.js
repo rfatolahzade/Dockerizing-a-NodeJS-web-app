48bcf2c45e32355b717b22d091fee70ba82de581
Mohammad Jamal Dashtaki <mjd.prg@gmail.com>
2022-01-14 09:24:11 +0100

    Init patternify

--- script/app.js
+++ script/app.js
@@ -1,30 +1,56 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var usersRouter = require("./routes/users");
-var app = express();
+const inquirer = require('inquirer')
+const fs = require('fs')
 
+const textQuestion = {
+  type: 'input',
+  name: 'text',
+  message: 'Please enter the text:'
+}
 
-app.get("/Exception", function mainHandler(req, res) {
-  throw new Error("An exception occured!");
-});
+const patternQuestion = {
+  type: 'input',
+  name: 'pattern',
+  message: 'Please enter the pattern:'
+}
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+function checkPattern(text, pattern, batchSize) {
+  const regexp = new RegExp(pattern, 'gi')
+  const allWords = text.split(' ')
+  const batches = []
+  let result = 0
 
+  while (allWords.length) {
+    batches.push(allWords.splice(0, batchSize))
+  }
 
-app.use("/users", usersRouter);
-app.get("/Hello", function rootHandler(req, res) {
-  res.end("Hello world!!");
-});
+  batches.map(batch => result += batch.join(' ').match(regexp)?.length || 0)
 
-app.use("/POW", function (req, res, next) {
-  res.send(Math.pow(req.query.a, req.query.b).toString());
-});
-var listener = app.listen(8080, function () {
-  console.log("Listening on port " + listener.address().port);
-});
+  return result
+}
+
+function getOcccurances(text, pattern) {
+  if (!Boolean(pattern)) {
+    return console.log(`0 occurrences of pattern found in the text.`)
+  }
+
+  const occurances = checkPattern(text, pattern, 4)
+
+  console.log(`${occurances} occurrences of pattern found in the text.`)
+}
+
+function main() {
+  try {
+    const data = fs.readFileSync('text.txt', 'utf8')
+
+    console.info('You have already provided your text in "text.txt"')
+
+    inquirer.prompt([patternQuestion]).then(answers => getOcccurances(data.toString(), answers['pattern']))
+
+  } catch (_) {
+    console.warn('There is not any text.txt file so you have to enter your text:')
+
+    inquirer.prompt([textQuestion, patternQuestion]).then(answers =>  getOcccurances(answers['text'], answers['pattern']))
+  }
+}
+
+main()
